c09673c81ace172fba262c501aa925a77af53692
Alex <alex@webage.com>
2024-05-10 16:02:03 -0400

    Added requirement for node 22+ in package.json. Minor button padding

--- people-app/src/ButtonBar.jsx
+++ people-app/src/ButtonBar.jsx
@@ -3,7 +3,7 @@
 export function ButtonBar({ reset, pickRandomPerson }) {
   return <section>
     <button className="btn btn-outline-danger" onClick={reset}>Reset</button>
-    <button className="btn btn-outline-success" onClick={pickRandomPerson}>Pick Random Person</button>
+    <button className="btn btn-outline-success m-2" onClick={pickRandomPerson}>Pick Random Person</button>
   </section>
 }
 
